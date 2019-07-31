3e5cdf826e917ea3f6f82da6c442a80f8f5639fa
Mario Nikolaus <mario.nikolaus@gmail.com>
2019-07-31 16:24:59 +0200

    updated demo file

--- demo/src/index.js
+++ demo/src/index.js
@@ -7,20 +7,25 @@ class Demo extends Component {
   constructor() {
     super();
     this.state = {
+      colors: ['blue'],
       value: 0,
+      showUnit: false,
     };
   }
   componentDidMount() {
-    setInterval(() => this.setState({ value: Math.round(Math.random() * 200) - 120}), 2000);
+    setInterval(() => this.setState({ value: Math.round(Math.random() * 200) - 120 }), 2000);
+    setInterval(() => this.setState({ showUnit: !this.state.showUnit }), 4000);
   }
   render() {
     return <div>
       <h1>react-advanced-gauge Demo</h1>
       <Gauge
+        colors={this.state.colors}
         height={600}
         min={-100}
         max={100}
         label="fkdsljaflkasdj"
+        showUnit={this.state.showUnit}
         thresholds={[20, 60]}
         unit="Hz"
         value={this.state.value}
